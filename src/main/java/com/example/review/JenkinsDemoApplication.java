4ce09142c7d64d12d3a82161f5a68cb08b074a46
Mark Kamradt <mark.kamradt@gmail.com>
2022-03-30 10:13:45 -0700

    first commit

---
+++ src/main/java/com/example/review/JenkinsDemoApplication.java
@@ -0,0 +1,27 @@
+package com.example.review;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class JenkinsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsDemoApplication.class, args);
+	}
+
+	@GetMapping("/sayHello")
+	public String getRequest0() {
+		return "Hello, world";
+	}
+
+	@GetMapping("/giveFeedback")
+	public String getRequest1() {
+		return "You suck";
+	}
+	
+}
+
